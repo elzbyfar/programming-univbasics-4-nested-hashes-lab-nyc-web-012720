d296000977a003f1cb7dc3ae0e8e98ff72167def
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-10 05:07:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,10 +79,13 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        }, 
+        :yukirio_matsumoto => {
+          :known_for => "Ruby", 
+          :languages => ["LISP", "C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 programmer_hash
 end
 
