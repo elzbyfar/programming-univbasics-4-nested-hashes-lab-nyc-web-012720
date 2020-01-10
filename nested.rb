a65b40814dc2978d6e60c99b831d1376b076c282
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-10 05:12:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,6 @@ def changing_alan
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      programmer_hash
-     
 end
 
 def adding_to_dennis
@@ -128,5 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     "Assembly".push(programmer_hash[:dennis_ritchie][:languages]) 
 
 end
